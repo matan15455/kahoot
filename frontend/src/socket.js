9fc68dc370291ce1dae3d0fa91c59f953359ccea
Matan Amram <matan15455@gmail.com>
2025-12-21 21:13:26 +0200

    add auth to socket in frontend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,10 @@
-// frontend/src/socket.js
 import { io } from 'socket.io-client';
 
 // התחברות לשרת Socket.IO
-export const socket = io('http://localhost:5000');
+const token = localStorage.getItem("token");
 
-// אפשר להוסיף כאן פונקציות עזר למשל:
-// export const joinRoom = (code, name) => socket.emit('joinRoom', { code, playerName: name });
-// export const createRoom = (hostName, game) => socket.emit('createRoom', { hostName, game });
+export const socket = io("http://localhost:5000", {
+  auth: {
+    token
+  }
+});
